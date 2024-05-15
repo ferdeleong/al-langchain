76b7fe37f7aca542c37bfa46f6b5df1d109d5343
Fernanda De León <fernandadeleon@Fernandas-MacBook-Pro.local>
2024-05-15 01:30:28 -0600

    reload existing vector and add more data and recreate agent

--- main.py
+++ main.py
@@ -44,10 +44,73 @@ db = DeepLake(dataset_path=dataset_path, embedding_function=embeddings)
 # add documents to our Deep Lake dataset
 db.add_documents(docs)
 
+# create a RetrievalQA chain
 retrieval_qa = RetrievalQA.from_chain_type(
 	llm=llm,
 	chain_type="stuff",
 	retriever=db.as_retriever()
 )
 
+# create an agent that uses the RetrievalQA chain as a tool
 
+tools = [
+    Tool(
+        name="Retrieval QA System",
+        func=retrieval_qa.run,
+        description="Useful for answering questions."
+    ),
+]
+
+agent = initialize_agent(
+	tools,
+	llm,
+	agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+	verbose=True
+)
+
+response = agent.run("When was Napoleone born?")
+print(response)
+
+# reloading an existing vector store and adding more data
+
+# load the existing Deep Lake dataset and specify the embedding function
+db = DeepLake(dataset_path=dataset_path, embedding_function=embeddings)
+
+# create new documents
+texts = [
+    "Lady Gaga was born in 28 March 1986",
+    "Michael Jeffrey Jordan was born in 17 February 1963"
+]
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+docs = text_splitter.create_documents(texts)
+
+# add documents to our Deep Lake dataset
+db.add_documents(docs)
+
+# instantiate the wrapper class for GPT3
+llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0)
+
+# create a retriever from the db
+retrieval_qa = RetrievalQA.from_chain_type(
+	llm=llm, chain_type="stuff", retriever=db.as_retriever()
+)
+
+# instantiate a tool that uses the retriever
+tools = [
+    Tool(
+        name="Retrieval QA System",
+        func=retrieval_qa.run,
+        description="Useful for answering questions."
+    ),
+]
+
+# create an agent that uses the tool
+agent = initialize_agent(
+	tools,
+	llm,
+	agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+	verbose=True
+)
+
+response = agent.run("When was Michael Jordan born?")
+print(response)
