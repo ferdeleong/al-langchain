25e932bf931df930ce6f6e2ce2bf37e96e83b334
Fernanda De León <fernandadeleon@Fernandas-MacBook-Pro.local>
2024-05-15 00:27:44 -0600

    Create Deep Lake dataset

---
+++ main.py
@@ -0,0 +1,44 @@
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import DeepLake
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.llms import OpenAI
+from langchain.chains import RetrievalQA
+
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+openai_project_id = os.getenv("OPENAI_PROJECT_ID")
+activeloop_token = os.getenv("ACTIVELOOP_TOKEN")
+
+os.environ["OPENAI_API_KEY"] = openai_api_key
+os.environ["OPENAI_PROJECT_ID"] = openai_project_id
+
+# Before executing the following code, make sure to have your
+# Activeloop key saved in the “ACTIVELOOP_TOKEN” environment variable.
+
+# instantiate the LLM and embeddings models
+llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0)
+embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+
+# create our documents
+texts = [
+    "Napoleon Bonaparte was born in 15 August 1769",
+    "Louis XIV was born in 5 September 1638"
+]
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+docs = text_splitter.create_documents(texts)
+
+# create Deep Lake dataset
+# TODO: use your organization id here. (by default, org id is your username)
+my_activeloop_org_id = "ferdleon" 
+my_activeloop_dataset_name = "langchain_course_from_zero_to_hero"
+dataset_path = f"hub://{my_activeloop_org_id}/{my_activeloop_dataset_name}"
+db = DeepLake(dataset_path=dataset_path, embedding_function=embeddings)
+
+# add documents to our Deep Lake dataset
+db.add_documents(docs)
+
