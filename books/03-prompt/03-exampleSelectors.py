eca624933ccc84e7bb247d56969d3d6fb4392384
Fernanda De León <fernandadeleon@Fernandas-MacBook-Pro.local>
2024-05-27 21:47:43 -0600

    Most relevant examples based on the input query

--- books/03-prompt/03-exampleSelectors.py
+++ books/03-prompt/03-exampleSelectors.py
@@ -6,6 +6,9 @@ from langchain.prompts.chat import (
     AIMessagePromptTemplate,
     HumanMessagePromptTemplate,
 )
+from langchain.prompts.example_selector import LengthBasedExampleSelector
+from langchain.prompts import FewShotPromptTemplate, PromptTemplate
+
 
 from dotenv import load_dotenv
 import os
@@ -26,3 +29,86 @@ human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
 chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, example_human, example_ai, human_message_prompt])
 chain = LLMChain(llm=chat, prompt=chat_prompt)
 chain.run("I love programming.")
+
+# create our examples
+examples = [
+    {
+        "query": "What's the weather like?",
+        "answer": "It's raining cats and dogs, better bring an umbrella!"
+    }, {
+        "query": "How old are you?",
+        "answer": "Age is just a number, but I'm timeless."
+    }
+]
+
+# create an example template
+example_template = """
+User: {query}
+AI: {answer}
+"""
+
+# create a prompt example from above template
+example_prompt = PromptTemplate(
+    input_variables=["query", "answer"],
+    template=example_template
+)
+
+# now break our previous prompt into a prefix and suffix
+# the prefix is our instructions
+prefix = """The following are excerpts from conversations with an AI
+assistant. The assistant is known for its humor and wit, providing
+entertaining and amusing responses to users' questions. Here are some
+examples:
+"""
+# and the suffix our user input and output indicator
+suffix = """
+User: {query}
+AI: """
+
+# now create the few-shot prompt template
+few_shot_prompt_template = FewShotPromptTemplate(
+    examples=examples,
+    example_prompt=example_prompt,
+    prefix=prefix,
+    suffix=suffix,
+    input_variables=["query"],
+    example_separator="\n\n"
+)
+
+chain = LLMChain(llm=chat, prompt=few_shot_prompt_template)
+chain.run("What's the secret to happiness?")
+
+examples = [
+    {"word": "happy", "antonym": "sad"},
+    {"word": "tall", "antonym": "short"},
+    {"word": "energetic", "antonym": "lethargic"},
+    {"word": "sunny", "antonym": "gloomy"},
+    {"word": "windy", "antonym": "calm"},
+]
+
+example_template = """
+Word: {word}
+Antonym: {antonym}
+"""
+
+example_prompt = PromptTemplate(
+    input_variables=["word", "antonym"],
+    template=example_template
+)
+
+example_selector = LengthBasedExampleSelector(
+    examples=examples,
+    example_prompt=example_prompt,
+    max_length=25,
+)
+
+dynamic_prompt = FewShotPromptTemplate(
+    example_selector=example_selector,
+    example_prompt=example_prompt,
+    prefix="Give the antonym of every input",
+    suffix="Word: {input}\nAntonym:",
+    input_variables=["input"],
+    example_separator="\n\n",
+)
+
+print(dynamic_prompt.format(input="big"))
